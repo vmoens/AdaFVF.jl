db83211848b8fc783e34e18a2f3c1f7e853f261a
Vincent Moens <Vincent.Moens@aig.com>
2018-11-23 09:31:41 +0000

    test on adam

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,24 @@
 using AdaFVF,Flux
 import Flux: Tracker, Optimise
 
+println("Testing grad update...")
+
 MyModel = Chain(Dense(30,30,σ),Dense(30,10))
 P = parameters(MyModel) 
-opt = AdaFVF.Adafvf(P)
+adafvf = AdaFVF.Adafvf(P)
 
 loss = sum(MyModel(randn(30,100))
 Flux.Tracker.back!(loss)
 
-opt()
-opt()
-opt()
+adafvf()
+
+println("Update time for 2-layer ANN...")
+t = @elapsed adafvf()
+println("done (took $t seconds).")
+
+println("Comparing with Adam:")
+adam = ADAM(P)
+adam()
+t = @elapsed adam()
+println("done (took $t seconds).")
 
