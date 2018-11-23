08a1a6e310b752d0e285fc9834b3baafae478b14
Vincent Moens <Vincent.Moens@aig.com>
2018-11-23 09:23:00 +0000

    export optimiser

--- src/AdaFVF.jl
+++ src/AdaFVF.jl
@@ -3,6 +3,8 @@ module AdaFVF
 using Flux, SpecialFunctions
 using Flux: Tracker, Optimise
 import Flux.Optimise: optimiser, invdecay, descent
+
+export Adafvf
 GRAD_SAMPLING = false
 NORMALIZED_DIFF = true
 NAN_CHECK = true
